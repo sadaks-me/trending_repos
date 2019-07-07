efac80044dc1474fe0f6df466c13fbb1cd40481f
nowapps <sadak@nowappstech.com>
2019-07-07 14:49:35 +0530

    Widget Test Added

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -5,26 +5,74 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:go_jek/main.dart';
+import 'package:go_jek/model/repo.dart';
+import 'package:go_jek/provider/home_provider.dart';
+import 'package:go_jek/view/widget/list_item.dart';
+import 'package:provider/provider.dart';
+import 'mock_image_http_client.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(MyApp());
+  // Mock object creation
+  Repo repo = new Repo(
+      author: "open-deepnude",
+      name: "open-deepnude",
+      avatar: "https://github.com/open-deepnude.png",
+      url: "https://github.com/open-deepnude/open-deepnude",
+      description: "Open source reverse-engineered clone of DeepNude",
+      language: "Python",
+      languageColor: "#3572A5",
+      stars: 915,
+      forks: 454,
+      currentPeriodStars: 109,
+      builtBy: []);
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
+  // Expected widget within created widgets
+  List<String> expectedTexts = [
+    repo.author,
+    repo.name,
+    repo.description,
+    repo.language,
+    repo.stars.toString(),
+    repo.forks.toString()
+  ];
 
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
+  testWidgets('Repo Detail Widget test', (WidgetTester tester) async {
+    HttpOverrides.runZoned(() async {
+      await tester.pumpWidget(
+        MultiProvider(
+          providers: [
+            ChangeNotifierProvider<HomeProvider>(
+                builder: (_) => HomeProvider()),
+          ],
+          child: MaterialApp(
+            home: ListView(
+              children: <Widget>[RepoListItem(repo, 0)],
+            ),
+          ),
+        ),
+      );
 
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+      // Gets the created widgets
+      Iterable<Widget> listOfWidgets = tester.allWidgets;
+      checkIfTextsCreatedCorrectly(listOfWidgets, expectedTexts);
+    }, createHttpClient: createMockImageHttpClient);
   });
+}
+
+// Method to check if all texts are created as expected.
+void checkIfTextsCreatedCorrectly(
+    Iterable<Widget> listOfWidgets, List<String> expectedTexts) {
+  var textWidgetPosition = 0;
+
+  for (Widget widget in listOfWidgets) {
+    if (widget is Text) {
+      expect(widget.data, expectedTexts[textWidgetPosition]);
+      textWidgetPosition++;
+    }
+  }
 }
